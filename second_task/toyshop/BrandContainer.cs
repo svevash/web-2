1590f7350c3b529099f2b06e062bbb0cac9cc574
Svetlana <vashchenko.siu@students.dvfu.ru>
2019-11-07 19:06:06 +1000

    new_files

---
+++ second_task/toyshop/BrandContainer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace toyshop
+{
+    public class BrandContainer
+    {
+        private Dictionary<Brand, List<int>> _brands;
+
+        public Dictionary<Brand, List<int>> Brands
+        {
+            get => _brands;
+            set => _brands = value;
+        }
+
+        public BrandContainer()
+        {
+            _brands = new Dictionary<Brand, List<int>>();
+        }
+    }
+}
---
+++ second_task/toyshop/MaterialContainer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace toyshop
+{
+    public class MaterialContainer
+    {
+        private Dictionary<Material, List<int>> _materials;
+
+        public Dictionary<Material, List<int>> Materials
+        {
+            get => _materials;
+            set => _materials = value;
+        }
+
+        public MaterialContainer()
+        {
+            _materials = new Dictionary<Material, List<int>>();
+        }
+    }
+}
