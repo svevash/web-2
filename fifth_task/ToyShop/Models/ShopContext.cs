2ebab7e694e11cf8e7a7ef2014b019baf4192a2e
Svetlana <vashchenko.siu@students.dvfu.ru>
2020-01-06 23:49:58 +1000

    add context

---
+++ fifth_task/ToyShop/Models/ShopContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToyShop.Models
+{
+    public class ShopContext : DbContext
+    {
+        public DbSet<Toy> Toys { get; set; }
+        public DbSet<ToyType> Types { get; set; }
+        public ShopContext(DbContextOptions options)
+            : base(options)
+        {
+            //Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+    }
+}
