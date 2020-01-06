699e4d22f271fd63d789296120fe971ac2d452da
Svetlana <vashchenko.siu@students.dvfu.ru>
2020-01-06 22:59:04 +1000

    start 5 add timestamp

---
+++ fifth_task/ToyShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ToyShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ fifth_task/ToyShop/Models/Toy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToyShop.Models
+{
+    public class Toy
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public TimeSpan LifeTime => DateTime.Now - CreationDate;
+        public int TypeId { get; set; }
+        public ToyType Type { get; set; }
+        private DateTime CreationDate { get; set; }
+    }
+}
